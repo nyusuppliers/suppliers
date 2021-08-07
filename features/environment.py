fef32ed2aa9d9b8826da63a67e72fe1c0a3f1f86
Wenhan Zhang <zhangwh5678@163.com>
2021-08-07 20:02:08 +0800

    update url

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ from os import getenv
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
+BASE_URL = getenv('BASE_URL', 'https://nyu-supplier-service-sum21.us-south.cf.appdomain.cloud')
 
 def before_all(context):
     """Executed once before all tests"""
